ca97708cf701c082c75fd192c29f51c0e2b865e6
samratk-cuelogic <samrat.kasture@cuelogic.com>
2019-06-24 19:33:43 +0530

    DB connection

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,6 @@
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
@@ -9,9 +12,25 @@ const functions = require('firebase-functions');
 
 
 
-// The Firebase Admin SDK to access the Firebase Realtime Database.
-const admin = require('firebase-admin');
-admin.initializeApp();
+// admin.initializeApp();
+// admin.initializeApp(functions.config().firebase);
+let serviceAccount = require('../serviceAccountKey.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://test-85780.firebaseio.com"
+  });
+
+
+let db = admin.firestore();
+
+let docRef = db.collection('cmsdb').doc('2');
+
+let setAda = docRef.set({
+  first: '2Ada',
+  last: '2Lovelace',
+  born: 1815
+});
 
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
@@ -19,7 +38,7 @@ exports.app = functions.https.onRequest(async (req, res) => {
     // Grab the text parameter.
     const original = "HELLO"; //req.query.text || "HELLO";
     // Push the new message into the Realtime Database using the Firebase Admin SDK.
-    // const snapshot = await admin.database().ref('/messages').push({original: original});
+    const snapshot = await admin.database().ref('/cmsdb').push({original: original});
     // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
     res.send(original);
    // res.redirect(303, snapshot.ref.toString());
